8dde5b68d62b8fc71692bc6a1abf25c6b6ea0db8
christofferwern <christoffer.wern@gmail.com>
2013-11-29 10:25:58 +0100

    Updated models. Some changes in css.

--- frontend/webpage/js/garage.js
+++ frontend/webpage/js/garage.js
@@ -14,41 +14,42 @@ $(document).ready(function() {
 	
 	//TODO - fix so this works with correct variable names, maybe make a function with an array 
 	//that contains {'chassi', 'wheels' ......} etc.
-	$("#weapon").droppable({
+	$("#weaponBox").droppable({
 		accept: ".weapon",
 		drop: function (event, ui) {
             if (ui.draggable.is('.dropped')) return false;
-			$("#weapon").text("");
+			$("#weaponBox").text("");
 			parent.components[2] = ui.draggable[0].id;
 			parent.actionNodes.push("weaponButton");
-            $("#weapon").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
+            $("#weaponBox").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
 			document.getElementById("robot3D").contentWindow.hasChanged(ui.draggable[0].id);
 		}
 	});
 	
-	$("#chassi").droppable({
+	$("#chassiBox").droppable({
 		accept: ".chassi",
 		drop: function (event, ui) {
             if (ui.draggable.is('.dropped')) return false;
-			$("#chassi").text("");
+			$("#chassiBox").text("");
 			parent.components[0] = ui.draggable[0].id;
 			parent.maxNrOfSensorNodes = 3;
 			//to show continuation with other chassis.
 			//if(ui.draggable[0].id == "chassi2") parent.maxnrOfSensorNodes = 5;
-            $("#chassi").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
+            $("#chassiBox").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
 			document.getElementById("robot3D").contentWindow.hasChanged(ui.draggable[0].id);
         }
 	});
 	
-	$("#wheels").droppable({
+	$("#wheelsBox").droppable({
 		accept: ".wheels",
 		drop: function (event, ui) {
             if (ui.draggable.is('.dropped')) return false;
-			$("#wheels").text("");
+			$("#wheelsBox").text("");
 			parent.actionNodes.push("wheelButton");
 			parent.components[1] = ui.draggable[0].id;
-            $("#wheels").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
-        }
+            $("#wheelsBox").append("<img src='Style/"+ui.draggable[0].id+".jpg' height='100px' width='100px'>");
+			document.getElementById("robot3D").contentWindow.hasChanged(ui.draggable[0].id);
+		}
 	});
 	
 	$( ".chassi").draggable({
@@ -106,28 +107,28 @@ function addComponents(){
 		parent.maxNrOfSensorNodes = 3;
 		//to show continuation with other chassis.
 		//if(ui.draggable[0].id == "chassi2") parent.maxnrOfSensorNodes = 5;
-		$("#chassi").text("");
-		$("#chassi").append("<img src='Style/"+parent.components[0]+".jpg' height='100px' width='100px'>");
+		$("#chassiBox").text("");
+		$("#chassiBox").append("<img src='Style/"+parent.components[0]+".jpg' height='100px' width='100px'>");
 	}
 	if(parent.components[1] != null){
 		parent.actionNodes.push("wheelButton");
-		$("#wheels").text("");	
-		$("#wheels").append("<img src='Style/"+parent.components[1]+".jpg' height='100px' width='100px'>");
+		$("#wheelsBox").text("");	
+		$("#wheelsBox").append("<img src='Style/"+parent.components[1]+".jpg' height='100px' width='100px'>");
 	}
 	if(parent.components[2] != null){
 		parent.actionNodes.push("weaponButton");
-		$("#weapon").text("");
-		$("#weapon").append("<img src='Style/"+parent.components[2]+".jpg' height='100px' width='100px'>");
+		$("#weaponBox").text("");
+		$("#weaponBox").append("<img src='Style/"+parent.components[2]+".jpg' height='100px' width='100px'>");
 	}
 	if(parent.components[3] != null){
 		//parent.actionNodes.push("weaponButton2"); tills vidare
-		$("#weapon").text("");
-		$("#weapon").append("<img src='Style/"+parent.components[3]+".jpg' height='100px' width='100px'>");
+		$("#weaponBox").text("");
+		$("#weaponBox").append("<img src='Style/"+parent.components[3]+".jpg' height='100px' width='100px'>");
 	}
 	if(parent.components[4] != null){
 		//parent.actionNodes.push("weaponButton3"); tills vidare
-		$("#weapon").text("");
-		$("#weapon").append("<img src='Style/"+parent.components[3]+".jpg' height='100px' width='100px'>");
+		$("#weaponBox").text("");
+		$("#weaponBox").append("<img src='Style/"+parent.components[3]+".jpg' height='100px' width='100px'>");
 	}
 }
 

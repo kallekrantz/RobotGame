c523313026720d5374e95bb1dcc667c9454e3004
unknown <mikpe201@ITN-K4507-08.ad.liu.se>
2013-12-10 14:20:47 +0100

    Connection functionality added to webpage, added some functionality in 3d-files aswell.

--- frontend/3d/testServer.js
+++ frontend/3d/testServer.js
@@ -6,7 +6,7 @@ userName = "bla";
  * @return 
  */
 matchMakerSocket.onopen = function() {
-	document.getElementById("messages").innerHTML+="Welcome!";
+	window.frames[0].document.getElementById("messages").innerHTML+="Welcome!";
 };
 
 /**
@@ -16,7 +16,7 @@ matchMakerSocket.onopen = function() {
  * @return 
  */
 matchMakerSocket.onmessage = function (evt) {
-    document.getElementById("debug").innerHTML+="<br>"+evt.data;
+    window.frames[0].document.getElementById("debug").innerHTML+="<br>"+evt.data;
 	if(evt.data.indexOf("port:") !== -1){
 		start = evt.data.indexOf(":")+1;
 		PORT=evt.data.substring(start,start+5);
@@ -39,7 +39,7 @@ function startNewMatch (port){
 	matchSocket.onopen = function() {
 		matchMakerSocket.close();
 		matchSocket.send("0"+makeReq(port,userName));
-		document.getElementById("messages").innerHTML="Messages: Loading scene";
+		window.frames[0].document.getElementById("messages").innerHTML="Messages: Loading scene";
 	}
 	/**
 	 * Handles messages from the matchsocket
@@ -54,14 +54,20 @@ function startNewMatch (port){
 			var startingState = JSON.parse(evt.data.replace("StartingState::",""));
 			currentMatchState = new NetworkMatch(startingState);
 			nextMatchState = new NetworkMatch(startingState);
-			document.getElementById('arena').innerHTML="<iframe src='arena.html' width='600px' height='500px'></iframe>";
+			console.log(window.frames[0].document);
+			
+			parent.changeFrame('battle.html');
+			console.log(window.frames[0].document);
+			
+			//window.frames[0].document.getElementById('arena').innerHTML="<iframe src='../3d/arena.html' width='600px' height='500px'></iframe>";
 		}else if(evt.data.indexOf("UpdateState::")!=-1){
 			var newState = JSON.parse(evt.data.replace("UpdateState::",""));
 			nextMatchState = new NetworkMatch(newState);
-			document.getElementById("debug").innerHTML+=" x: " + nextMatchState.robots[0].getX() +" dx: " + nextMatchState.robots[0].getdX() + " y: " + nextMatchState.robots[0].getZ() + " dY: " + nextMatchState.robots[0].getdZ()+"<br>"+" rot: "+nextMatchState.robots[0].getRotation()+" angVel: "+nextMatchState.robots[0].getAngularVelocity()+"<br>";
+			window.frames[0].document.getElementById("debug").innerHTML+=" x: " + nextMatchState.robots[0].getX() +" dx: " + nextMatchState.robots[0].getdX() + " y: " + nextMatchState.robots[0].getZ() + " dY: " + nextMatchState.robots[0].getdZ()+"<br>"+" rot: "+nextMatchState.robots[0].getRotation()+" angVel: "+nextMatchState.robots[0].getAngularVelocity()+"<br>";
+			window.frames[0].document.getElementById("messages").innerHTML="Messages: Game is on man!!!";
 		}else if(evt.data.indexOf("MatchEnded::")!=-1){
-			document.getElementById("messages").innerHTML="Messages: Game over man, game over";
-			document.getElementById("debug").innerHTML+="<br>"+evt.data;
+			window.frames[0].document.getElementById("messages").innerHTML="Messages: Game over man, game over";
+			window.frames[0].document.getElementById("debug").innerHTML+="</br>"+evt.data;
 			matchSocket.close();
 		}
 		//document.getElementById("debug").innerHTML+="   "+evt.data;
@@ -105,11 +111,11 @@ matchMakerSocket.onerror = function(err) {
  * @return 
  */
 function makeMatchMakerRequest(){
-	userName = document.getElementById("username").value;
-	matchType  = document.getElementById("matchtype").selectedIndex;
+	userName = window.frames[0].document.getElementById("username").value;
+	matchType  = window.frames[0].document.getElementById("matchtype").selectedIndex;
 	var message=JSON.stringify(new request(userName,12,matchType));
 	matchMakerSocket.send(message);
-	document.getElementById("messages").innerHTML="Messages: Entering "+document.getElementById("matchtype").options[document.getElementById("matchtype").selectedIndex].value+" lobby";
+	window.frames[0].document.getElementById("messages").innerHTML="Messages: Entering "+window.frames[0].document.getElementById("matchtype").options[window.frames[0].document.getElementById("matchtype").selectedIndex].value+" lobby";
 }
 /**
  * Request to join the matchhandler associated with the connected port.
@@ -162,6 +168,6 @@ function sendKey(key){
  * @return 
  */
 function setReady(){
-	document.getElementById("messages").innerHTML="Messages: Waiting for other players";
+	window.frames[0].document.getElementById("messages").innerHTML="Messages: Waiting for other players";
 	matchSocket.send("1");
 }
